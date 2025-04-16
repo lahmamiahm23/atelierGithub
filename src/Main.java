3e49a71fcdd4a0607aab902c771130c6ad44c648
Basraoui Mouad <basraouimoad0@gmail.com>
2025-04-16 10:40:11 +0100

    code main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-           System.out.println("i = " + i);
-        }
-        System.out.println("Bonjour, je suis le testeur");
+        System.out.println("Bonjour, je suis le main");
     }
 }
